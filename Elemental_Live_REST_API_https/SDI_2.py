d23feb540de0fa185f2dae7ced66fc294490aa97
YongHa-Shin <foolemperor92@gmail.com>
2022-10-24 15:06:36 +0900

    2022.10.24

--- Elemental_Live_REST_API_https/SDI_2.py
+++ Elemental_Live_REST_API_https/SDI_2.py
@@ -7,15 +7,15 @@ urllib3.disable_warnings() # https 인증서 관련 경고문 제거
 
 enc_ip = '10.20.22.139' #Elemental Live: IP
 enc_user = 'encoder' #Elemental Live: USER ID
-enc_key = 'y5jYbKPpsnpt1bjs_MVp' #Elemental Live: Authentication Key
+enc_key = 'x3QojFkUjvkkojybswS7' #Elemental Live: Authentication Key
 
 enc_img_path_right = '/data/ftpuser/image/right_top_logo2.png' # 우상단_리플레이.png, right_top_logo.png, right_top_logo2.png
 enc_img_path_left = '/data/ftpuser/image/image_null.png'        # image_null.png, left_top.png
 enc_img_layer_0 = '<layer>0</layer>'
 enc_file_path = '/data/ftpuser/archive/Live_Ending.mp4'
 
-enc_event = '17'                 #Elemental Live Event ID
-enc_input_id = '28'                   #Elemental INPUT ID
+enc_event = '3'                 #Elemental Live Event ID
+enc_input_id = '4'                   #Elemental INPUT ID
 
 ELR = Elemental_API(enc_ip, enc_user, enc_key, enc_event)
 
--- Elemental_Live_REST_API_https/SDI_5.py
+++ Elemental_Live_REST_API_https/SDI_5.py
@@ -7,14 +7,14 @@ urllib3.disable_warnings() # https 인증서 관련 경고문 제거
 
 enc_ip = '10.20.22.139' #Elemental Live: IP
 enc_user = 'encoder' #Elemental Live: USER ID
-enc_key = 'y5jYbKPpsnpt1bjs_MVp' #Elemental Live: Authentication Key
+enc_key = 'x3QojFkUjvkkojybswS7' #Elemental Live: Authentication Key
 
 enc_img_path_right = '/data/ftpuser/image/image_null.png' # 우상단_리플레이.png, right_top_logo.png, right_top_logo2.png
 enc_img_path_left = '/data/ftpuser/image/image_null.png'        # image_null.png
 enc_img_layer_0 = '<layer>0</layer>'
 
-enc_event = '34'                 #Elemental Live Event ID
-enc_input_id = '59'                   #Elemental INPUT ID
+enc_event = '5'                 #Elemental Live Event ID
+enc_input_id = '8'                   #Elemental INPUT ID
 
 ELR = Elemental_API(enc_ip, enc_user, enc_key, enc_event)
 
